1fec8ef149cb7688d5247248fc303af844082641
Richard Swinkels <github@richardswinkels.nl>
2024-02-03 08:17:33 +0100

    Setup migrations

--- database/migrations/2024_02_02_111854_create_assemblies_table.php
+++ database/migrations/2024_02_02_111854_create_assemblies_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('assemblies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->boolean('is_admin');
-            $table->rememberToken()->nullable();
+            $table->string('image')->nullable();
+            $table->float('price');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('assemblies');
     }
 };
---
+++ database/migrations/2024_02_02_112403_create_assembly_components_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assembly_components', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('assembly_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('component_id')->constrained()->cascadeOnDelete();
+            $table->string('location', 10);
+
+            $table->unique([
+                'component_id',
+                'location',
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assembly_components');
+    }
+};
