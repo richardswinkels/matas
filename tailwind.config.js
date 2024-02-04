a7b0a31eaae82f8776ae96d6d1ca3b6d27f47c8f
Richard Swinkels <github@richardswinkels.nl>
2024-02-04 18:39:28 +0100

    Install tailwind forms and laravel pagination package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [forms],
+};
